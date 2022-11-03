51afcae64646cfb8f8adcd136dff9da8730a5fed
Annie <115146344+AnnieOhlen@users.noreply.github.com>
2022-11-03 01:55:35 +0100

    Add files via upload

---
+++ tennisScript.js
@@ -0,0 +1,12 @@
+// Single line comment.
+
+/* Comment block */
+
+/*
+"use strict";
+The "use strict" directive is only recognized at the beginning of a script or a function.
+Strict mode makes it easier to write "secure" JavaScript.
+Strict mode changes previously accepted "bad syntax" into real errors.
+https://www.w3schools.com/js/js_strict.asp
+*/
+
