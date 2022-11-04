0402a7c921a3056e41dcb5af43218a2e59b30b56
Annie <115146344+AnnieOhlen@users.noreply.github.com>
2022-11-04 02:27:17 +0100

    Add files via upload

--- tennisScript.js
+++ tennisScript.js
@@ -1,12 +1,22 @@
-// Single line comment.
-
-/* Comment block */
-
 /*
+Comments:
 "use strict";
 The "use strict" directive is only recognized at the beginning of a script or a function.
 Strict mode makes it easier to write "secure" JavaScript.
 Strict mode changes previously accepted "bad syntax" into real errors.
 https://www.w3schools.com/js/js_strict.asp
+
+Ideas for script:
+- If clicked x amount of times on headers then print out: are you lost? Take this map!
+Some kind of function with CSS animation connected to it?
+- Function; if window width less than X change the font-size and table sized down? First letter only?
+- Being able to change between bright & dark theme with button (bright currently standard for the website).
 */
+
+
+//Script _only_ after this comment:
+
+
+
+
 
