f4dbf5317dd9cacf06622952ac66507196b72797
Annie <115146344+AnnieOhlen@users.noreply.github.com>
2022-12-11 22:47:13 +0100

    Broken js..

--- tennisScript.js
+++ tennisScript.js
@@ -1,42 +1,5 @@
-//'use strict';
-
 //Button at the bottom of the page to get back to the top.
-
   function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
   }
-
-
-
-
-/*
-function changeTheme() {
-    var element = document.body;
-    element.classList.toggle("darkMode")
-}
-*/
-
-
-
-const setTheme = theme => document.documentElement.className = theme;
-
-
-
-/*function sendReservation() {
-    var input = document.getElementById("");
-    sessionStorage.setItem("", input.value);
-    return true;
-}
-*/
-
-
-
-/*
-if (typeof(Storage) !== "undefined") {
-   Code for localStorage/sessionStorage.
-
-    } else {
-    Sorry! No Web Storage support..
-}
-*/
