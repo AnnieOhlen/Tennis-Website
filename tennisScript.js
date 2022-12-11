3d351fef44142d7511cadae7303ae7e0ead75eda
Annie <115146344+AnnieOhlen@users.noreply.github.com>
2022-12-11 19:55:39 +0100

    .

--- tennisScript.js
+++ tennisScript.js
@@ -1,21 +1,44 @@
+//"use strict";
+
+//Button at the bottom of the page to get back to the top.
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
+
+
+
+
+
+
 /*
-Comments:
-"use strict";
-The "use strict" directive is only recognized at the beginning of a script or a function.
-Strict mode makes it easier to write "secure" JavaScript.
-Strict mode changes previously accepted "bad syntax" into real errors.
-https://www.w3schools.com/js/js_strict.asp
-
-Ideas for script:
-- If clicked x amount of times on headers then print out: are you lost? Take this map!
-Some kind of function with CSS animation connected to it?
-- Being able to change between bright & dark theme with button (bright currently standard for the website).
-*/
-
-//Script _only_ after this comment:
-
 function changeTheme() {
     var element = document.body;
     element.classList.toggle("darkMode")
 }
+*/
 
+
+
+
+
+
+
+/*function sendReservation() {
+    var input = document.getElementById("");
+    sessionStorage.setItem("", input.value);
+    return true;
+}
+*/
+
+
+
+/*
+if (typeof(Storage) !== "undefined") {
+   Code for localStorage/sessionStorage.
+
+    } else {
+    Sorry! No Web Storage support..
+}
+*/
